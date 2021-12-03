c7f3ccf356db45b7699319971c20d878ce0531ad
Matteo Pompei <datxizzan@gmail.com>
2021-12-03 17:47:49 +0100

    CSS opzionale aggiunto

--- treni.js
+++ treni.js
@@ -1,16 +1,20 @@
-// prompt("Benvenuto nel calcolatore di prezzi dei biglietti del treno!");
+alert("Benvenuto nel calcolatore di prezzi dei biglietti del treno!");
 
 let distance = prompt("Quanti kilometri vuoi percorrere?");
 let age = prompt("Quanti anni hai?");
 let price = distance * 0.21;
 
-if (age < 18) {
+if (age <= 0) {
+  alert("Forse non hai ben capito la domanda... Riproviamo!");
+  location.reload();
+}
+else if (age < 18) {
   price = price - ((price / 100) * 20);
 }
 else if (age >= 65) {
   price = price - ((price / 100) * 40);
 }
 
-price = price.toFixed(2);
+document.getElementById("prezzo-biglietto").innerHTML = price;
 
-document.getElementById("prezzo-biglietto").innerHTML = "Il prezzo del tuo biglietto è di €" + price;
+price = price.toFixed(2);
